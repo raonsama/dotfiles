8c6f476c667e9bb5bb08cf503392c1bce636e848
Raymond M <159627856+raonsama@users.noreply.github.com>
2026-03-12 20:22:20 +0800

    Update setup.sh for tmux configuration and package list
    Added .tmux.conf copy command and updated package installation.

--- setup.sh
+++ setup.sh
@@ -8,9 +8,10 @@ cp -r .bashrc $HOME
 cp -r .gitconfig $HOME
 cp -r .termux $HOME
 cp -r .tmux $HOME
+cp -r .tmux.conf $HOME
 cp -r nvim $HOME/.config/
 
-termux-reload-settings
-termux-setup-storage
+apt update
+apt full-upgrade -y
 
-apt install neovim lazygit clang build-essential ripgrep fd wget curl fzf bash-completion composer nodejs golang php php-gd php-imagick php-ldap tree-sitter
+apt install tmux neovim lazygit clang build-essential ripgrep fd wget curl fzf bash-completion composer nodejs golang php php-gd php-imagick php-ldap tree-sitter
