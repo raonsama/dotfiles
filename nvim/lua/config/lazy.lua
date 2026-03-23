83e6c04631c7cf2d7b7048b2dfea1f562f8a3bc7
Raymond M <159627856+raonsama@users.noreply.github.com>
2026-03-24 04:44:34 +0800

    Add markdown language support to lazy configuration

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.util.dot" },
